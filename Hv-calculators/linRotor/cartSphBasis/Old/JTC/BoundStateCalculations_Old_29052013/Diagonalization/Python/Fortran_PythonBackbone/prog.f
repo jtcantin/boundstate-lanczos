3b97de29f38827edb24aa29dd54579c89e4640ef
Joshua Tyler Cantin <jtcantin@uwaterloo.ca>
2013-07-19 18:19:41 -0400

    Finished Potential Shuffling and Code Reorganization

--- Hv-calculators/linRotor/cartSphBasis/Old/JTC/BoundStateCalculations_Old_29052013/Diagonalization/Python/Fortran_PythonBackbone/prog.f
+++
@@ -1,9 +0,0 @@
-        program tester
-         integer i
-         character(len=32) arg
-
-         do i = 1, argc()
-            call getarg(i, arg)
-            write(*,*) arg
-         end do
-        end program
