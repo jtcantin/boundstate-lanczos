13e837b882611541081fd17551eecaef7c1ee69d
Joshua Tyler Cantin <jtcantin@uwaterloo.ca>
2013-08-27 14:35:33 -0400

    A larger basis seems slightly slower, but only about 1percent, which may be within the variance. There is a higher average CPU usage, however; so I will go with parallelized index shuffling in Vv

--- Compilation/tobyRun2.sh
+++ Compilation/tobyRun2.sh
@@ -1,7 +1,7 @@
 
 export OMP_NUM_THREADS=8
 echo Alavi_SPCE_smallCage11_ortho_x04_dx002_l5_tp70_cp18
-time ./LanczosTOBY LanczosInputFileToby.txt HvInputFileToby12.txt > /scratch/jtcantin/Alavi_SPCE_smallCage11_ortho_x04_dx002_l5_tp70_cp18
+time ./LanczosTOBY LanczosInputFileToby.txt HvInputFileToby12.txt > /scratch/jtcantin/Alavi_SPCE_smallCage11_ortho_x04_dx002_l5_tp70_cp18_parallelTest
 
 export OMP_NUM_THREADS=8
 echo SmallBasisTest
